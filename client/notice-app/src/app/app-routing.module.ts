5e9a9f8439d3a06d99662d8466490f183423d512
pshoultz <pshoultzula@gmail.com>
2023-01-15 02:58:15 -0500

    routing for component

--- client/notice-app/src/app/app-routing.module.ts
+++ client/notice-app/src/app/app-routing.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
-import { CmsImagesComponent } from './cms-images/cms-images.component';
 import { CmsComponent } from './cms/cms.component';
 
 const routes: Routes = [
     {path: 'cms', component: CmsComponent},
-    {path: 'cms/images', component: CmsImagesComponent, outlet: "cms-router"}
 ];
 
 @NgModule({
---
+++ client/notice-app/src/app/cms/cms-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CmsImagesComponent } from '../cms-images/cms-images.component';
+
+const routes: Routes = [
+  {path: 'cms/images', component: CmsImagesComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class CmsRoutingModule { }
