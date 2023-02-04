1f6e686368fed8b6e00df0991203bdd605328e98
pshoultz <pshoultzula@gmail.com>
2023-02-03 20:51:15 -0500

    done

--- client/notice-app/src/app/cms-broadcast/cms-broadcast.component.ts
+++ client/notice-app/src/app/cms-broadcast/cms-broadcast.component.ts
@@ -9,8 +9,17 @@ import { Component } from '@angular/core';
 export class CmsBroadcastComponent {
   ads: Array<string> = ['assets/htE0KtNl.jpeg', 'assets/HUlp2wQl.jpeg', 'assets/vM4dsRJl.jpg'];
   currentImg: string = ""
+  currentIdx = 0;
+  currentHeight = 0;
 
   public cast() {
-    this.currentImg = this.ads[0];
+    this.currentIdx++;
+    setTimeout(function() {
+      scroll();
+    }, 100)
+  }
+
+  private scroll() {
+    this.currentHeight += 10;
   }
 }
