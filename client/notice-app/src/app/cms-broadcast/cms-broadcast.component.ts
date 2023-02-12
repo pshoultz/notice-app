c4c38ecb9bd6e699215f01b43c4425b28d6f8f70
pshoultz <pshoultzula@gmail.com>
2023-02-12 03:11:21 -0500

    wow chatgpt ftw

--- client/notice-app/src/app/cms-broadcast/cms-broadcast.component.ts
+++ client/notice-app/src/app/cms-broadcast/cms-broadcast.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+
 
 @Component({
   selector: 'app-cms-broadcast',
@@ -8,18 +9,25 @@ import { Component } from '@angular/core';
 
 export class CmsBroadcastComponent {
   ads: Array<string> = ['assets/htE0KtNl.jpeg', 'assets/HUlp2wQl.jpeg', 'assets/vM4dsRJl.jpg'];
-  currentImg: string = ""
-  currentIdx = 0;
-  currentHeight = 0;
+  @ViewChild('canvas', { static: true }) canvas!: ElementRef<HTMLCanvasElement>;
+  private ctx!: any;
+  private image!: HTMLImageElement;
+  private y = 0;
+  private direction = 1;
+  private speed = 1;
 
-  public cast() {
-    this.currentIdx++;
-    setTimeout(function() {
-      scroll();
-    }, 100)
+  ngAfterViewInit() {
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+    this.image = new Image();
+    this.image.src = 'assets/htE0KtNl.jpeg';
+    this.image.onload = () => this.animate();
   }
 
-  private scroll() {
-    this.currentHeight += 10;
+  private animate() {
+    this.ctx.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+    this.ctx.drawImage(this.image, 0, this.y, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+    this.y -= this.direction * this.speed;
+
+    requestAnimationFrame(() => this.animate());
   }
 }
